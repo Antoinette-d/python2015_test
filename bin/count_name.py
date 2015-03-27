c9acaaf890227859e51497b02ade131795aa37dc
Antoinette-d <Antoinette-d@mweb.co.za>
2015-03-27 10:09:06 +0200

    Updated PythonTest.ipynb and bin/count_name.py

--- bin/count_name.py
+++ bin/count_name.py
@@ -1,19 +1,24 @@
-#!/usr/bin/eenv python
+#!/usr/bin/env python
 
 import sys
 
-def count_name(filename, protein_name):
-    input_file = open(sys.argv[1])
-    count = 0
-    for line in input_file:
-        if line.rstrip() == protein_name:
-            count += 1
-    return count
-
 if len(sys.argv) != 3:
-    sys.exit("Usage: count_name.py <protein names file> <protein name>")
+        sys.exit("Usage: count_name.py expects a filename and protein name")
 
-filename = sys.argv[0]
-protein_name = sys.argv[1]
+def count_name(filename, protein_name):
+    try:
+        input_file = open(filename)
+    except IOError as e:
+        print >>sys.stderr, "failed to open {}: {}".format(filename,e.stderr)
+        return False
+    else:
+        count = 0
+        for line in input_file:
+                if line.rstrip() == protein_name:
+                        count += 1
+        return count
+
+filename = sys.argv[1]
+protein_name = sys.argv[2]
 name_count = count_name(filename, protein_name)
 print protein_name, name_count
